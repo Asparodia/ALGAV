c68fa9034842c14243b679039293a2cd9921a47d
Asparodia <thiernobah91860@yahoo.fr>
2018-10-31 19:55:36 +0100

    ajout de SupprMin pour les tableau

--- tasMinTab.py
+++ tasMinTab.py
@@ -30,14 +30,37 @@ class TasTab:
                 pere = (i-1)//2
                 if(pere<0):
                     pere = 0
-                
+    def SupprMin(self):
+        
+        if(len(self.tab) == 0):
+            print("nothing in this heap")
+            return null
+        if(len(self.tab)==1):
+            res = self.tab[0]
+            self.tab = arange(0)
+            return res
+        if(len(self.tab) == 2):
+            res = self.tab[0]
+            self.tab = array(self.tab[1])
+            return res
+        else:
+            res = self.tab[0]
+            copie = self.tab
+            taille = len(self.tab)
+            self.tab = arange(0)
+            self.tab = append(self.tab,min(copie[1],copie[2]))
+            self.tab = append(self.tab,max(copie[1],copie[2]))
+            i =3
+            while (i < taille-1):
+                self.tab = append(self.tab,copie[i])
+                i = i+1
+            return res
 
-    def showMe(self):
+    def showMe(self):  
         for i in self.tab:
             print(i)
 
-
-
+        
 tab = TasTab()
 tab.Ajout(5)
 tab.Ajout(10)
@@ -46,3 +69,7 @@ tab.Ajout(20)
 tab.Ajout(30)
 tab.Ajout(4)
 tab.showMe()
+"""print("Supp min : ")
+print(tab.SupprMin())
+print("\n")
+tab.showMe()"""
