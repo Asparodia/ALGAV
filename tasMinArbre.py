9488624832d5344cd46fb55a5d227d911fed026a
Asparodia <thiernobah91860@yahoo.fr>
2018-11-26 19:09:20 +0100

    mesures de temps pour consIter pour tous les fichiers

--- tasMinArbre.py
+++ tasMinArbre.py
@@ -10,48 +10,38 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
-            
 
     def Ajout(self, data):
+# Compare the new value with the parent node
         if self.data:
-            if data > self.data:
+            if data < self.data:
                 if self.left is None:
                     self.left = Node(data)
-                elif self.right is None:
-                    self.right = Node(data)
                 else:
                     self.left.Ajout(data)
-                
-            elif(data<self.data):
-                res = self.data
-                self.data = data
-                self.Ajout(res)
-            else:
-                return False
-                
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.Ajout(data)
         else:
             self.data = data
 
-    def prefixePrint(self):
+# Print the tree
+    def PrintTree(self):
         if self.left:
-            self.left.prefixePrint()
-        if self.right:
-            self.right.prefixePrint()
-        print(self.data)
+            self.left.PrintTree()
         
+        if self.right:
+            self.right.PrintTree()
+        print( self.data),
+
+# Use the insert method to add nodes
 
 root = Node(2)
 root.Ajout(5)
 root.Ajout(6)
 root.Ajout(10)
-
 root.Ajout(13)
 root.Ajout(8)
-"""root.Ajout(7)
-root.Ajout(12)
-root.Ajout(15)
-root.Ajout(14)
-"""
-
-print("hauteur " + str(root.Hauteur()))
-root.prefixePrint()
+root.PrintTree()
