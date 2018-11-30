b983ae79e0eb31a04abf000e43b2636d1132d85b
Asparodia <thiernobah91860@yahoo.fr>
2018-11-30 14:51:08 +0100

    c'est mieux pour les mesures je pense faut que je test encore les mesures sur les pc de la fac le mien assume moyen

--- MesureTasMinConsIter.py
+++ MesureTasMinConsIter.py
@@ -15,33 +15,35 @@ def mesureTemps(fun,param1):
     end = time.time()
     return (end-start)
 
-def mesure(fun,file):
-    param = list()
-    f= open(file,'r')
-    for line in f:
-        param.append(cle.Cle(line))    
-    return mesureTemps(fun,param)
+def mesureConsIter(allFiles):
+    allFiles.sort()
+    
+    res = list()
+    
+    for x in allFiles:
+        j = 0 
+        time = 0
+        param = list()
+        print(x)
+        f = open("cles_alea/"+x,'r')
+        for line in f:
+            param.append(cle.Cle(line))
+            
+        a = tasMinTab.TasTab()
+        time = time + mesureTemps(a.ConsIter,param)
+
+        j = j + 1
+        res.append((j,x,time))
+        param = list()
+    return res
         
 
-a=tasMinTab.TasTab()
-print(mesure(a.ConsIter,"cles_alea/jeu_1_nb_cles_100.txt"))
-
 allFiles = os.listdir("cles_alea")
-allFiles.sort()
 
-Res = list()
-i = 0
-for x in allFiles:
-    a=tasMinTab.TasTab()
-    Res.append((i,mesure(a.ConsIter,"cles_alea/"+x),x))
-    i = i+1
+Res = mesureConsIter(allFiles)
 
-csvfileTime = "time.csv"
+csvfileTime = "ConsIterTime.csv"
 with open(csvfileTime,"w") as output:
     writer = csv.writer(output,lineterminator='\n')
     writer.writerows(Res)
 print(Res)
-total = 0
-for x in Res:
-    total = total + x[1]
-print("total time pour nos ficher avec la fonction ConsIter= "+str(total))
