ed1b074266dec52dbccd80107021d07cc7269393
Asparodia <thiernobah91860@yahoo.fr>
2018-12-09 17:46:22 +0100

    File Bino la complexite a pas l'air ouf et les files sont ptete pas bien ranger X) c'est chaud a verif en tout cas c'est l'algo du prof et ca l'air de marcher faudra faire les mesures qui on l'air galere

--- FileBino2.py
+++ FileBino2.py
@@ -2,110 +2,164 @@ import cle
 
 class TournoisBino:
     
-    def __init__(self):
-        self.keyNode = None
-        self.enfants = [self] #le premier enfant c'est enfants[1]
+    def __init__(self,root):
+        self.racine = root
+        self.enfants = [] #chaque enfant est un tournois de degree plus petit que le tournois actel
         self.degre = 0
-        self.ind = 0
+        #☺self.ind = 0
+    
+    def decapite(self):
+        res = self.racine
+        if(len(self.enfants)>1):
+            self.degre = self.degre - 1
+            self.racine = self.enfants[0].racine
+            self.enfants.pop(0)
+            return res
+        self.racine = None
+        return res
+    
+    def estVide(self):
+        if(self.racine is None):
+            return True
+        return False
+    
+    def file(self):
+        res = FileBinomial()
+        res.AjoutTournois(self)
+        return res
+    
+    def __repr__(self):
+        res = "Tournois Binomial : "
+        res = res +" racine : " + str(self.racine) 
+        res = res +" degre : [" + str(self.degre) +"]"
+        return res
 
-    def Ajout(self,add):
-        
-        self.enfants.append(add)
-        self.degre = self.degre + 1
-        #self.remonte(self.degre)
-   
-    def ConsIter(self, vals):
-	#if(len(vals) != math.pow(2,math.floor(math.log(len(vals),2)))):
-	#	return None
-        
-        self.key = vals[0]
-        self.enfants[0] = self
-        for i in range(1,len(vals)):
-            self.Ajout(vals[i])
-
-    def getDegre(self):
-        return degree
-
-    """def remonte(self, ind):
-        while ind != 0:
-            if (self.enfants[ind//2].keyNode.sup(self.tas[ind].keyNode)):
-                tmp = self.tas[ind//2]
-                self.tas[ind//2] = self.tas[ind]
-                self.tas[ind] = tmp
-                ind = ind//2
-            else:
-                break
-
-    def descend(self,ind):
-        while ((ind * 2) <= self.degre):
-            if (ind * 2 + 1 > self.degre):
-                minFils = ind * 2
-            else:
-                if( self.enfants[ind*2].keyNode.inf( self.tas[ind*2+1].keyNode)):
-                    minFils = ind * 2
-                else:
-                    minFils = ind * 2 + 1
-            
-            if (self.tas[minFils].keyNode.inf(self.tas[ind].keyNode)):
-                tmp = self.tas[ind]
-                self.tas[ind] = self.tas[minFils]
-                self.tas[minFils] = tmp
-
-            ind = minFils"""
     
 class FileBinomial:
     def __init__(self):
         self.tournois = []
         
-    def Union(self, h):
-        self.combine_roots(h)
-        if self.tournois == []:
+    def estVide(self):
+        if(self.tournois == []):
+            return True
+        return False
+    
+    def minDegre(self):
+        if(self.estVide()):
             return None
-        i = 0
-        while i < len(self.tournois)-1:
-            current = self.tournois[i]
-            after = self.tournois[i + 1]
-            if current.degre == after.degre:
-                if (i + 1 < len(self.tournois) - 1 and self.tournois[i + 2].degre == after.degre):
-                    after_after = self.tournois[i + 2]
-                    if after.key.inf(after_after.key):
-                        after.Ajout(after_after)
-                        del self.tournois[i + 2]
-                    else:
-                        after_after.Ajout(after)
-                        del self.tournois[i + 1]
-                else:
-                    if current.key.inf(after.key):
-                        current.Ajout(after)
-                        del self.tournois[i + 1]
-                    else:
-                        after.Ajout(current)
-                        del self.tournois[i]
-            i = i + 1
-            
-    def combine_roots(self, h):
-        self.tournois.extend(h.tournois)
         self.tournois = sorted(self.tournois,key = lambda tournoi : tournoi.degre)
-
+        return self.tournois[0]
+    
+    def reste(self):
+        if(self.estVide()):
+            
+            return None
+        self.tournois = sorted(self.tournois,key = lambda tournoi : tournoi.degre)
+        self.tournois.pop(0)
+        return self
+    
     def SuppMin(self):
         if self.tournois == []:
             return None
+        
         tournoisP = self.tournois[0]
-        for t in self.tournois:
-            if t.key.inf(tournoisP.key):
-                tournoisP = t
-        self.tournois.remove(tournoisP)
+        self.tournois.pop(0)
         h = FileBinomial()
-        h.tournois = tournoisP.enfants
-        self.Union(h)
+        for t in tournoisP.enfants:
+            h.AjoutTournois(t)
+            
+        res = UnionFile(self,h)
+        self.tournois = res.tournois
  
-        return tournoisP.key
+        return tournoisP.racine
 
 
     def AjoutTournois(self, t):
-        f = FileBinomial()
-        f.tournois.append(t)
-        self.Union(f)
+        if(self.tournois == []):
+            self.tournois.append(t)
+        else:
+            f = FileBinomial()
+            f.AjoutTournois(t)
+            res = UnionFile(self,f)
+            self.tournois = res.tournois
+            
+            
+    def ConsIter(self,tournois):
+        for t in tournois:
+            self.AjoutTournois(t)
+        
+    def __repr__(self):
+        res = "File Binomiale : < "
+        for x in self.tournois:
+            res = res + "TB" + str(x.degre) +" "
+        res = res + ">"
+        return res
+        
+def union2Tid(t1,t2):
+        if(t1.estVide() or t2.estVide()):
+            return None
+        rac1 = t1.racine
+        rac2 = t2.racine
+        if(rac1.inf(rac2)):
+            t1.enfants.append(t2)
+            t1.racine = rac1
+            t1.degre = t1.degre + 1
+            return t1
+        else:
+            t2.enfants.append(t1)
+            t2.racine = rac2
+            t2.degre = t1.degre + 1
+            return t2
+
+def UnionFile(F1,F2):
+        t = TournoisBino(None)
+        return Union(F1,F2,t)
+
+def ajoutMin(F,tournoi):
+        if(F.minDegre().degre<tournoi.degre):
+            return None
+        F.tournois.insert(0,tournoi)
+        return F
+        
+def Union(F1,F2,t):
+        
+    if(t.estVide()):
+        
+        if(F1.estVide()):
+            
+            return F2
+            
+        if(F2.estVide()):
+            
+            return F1
+        
+        t1 = F1.minDegre()
+        t2 = F2.minDegre()
+        
+        if(t1.degre<t2.degre):
+            return ajoutMin(UnionFile(F1.reste(),F2),t1)
+        if(t2.degre<t1.degre):
+            return ajoutMin(UnionFile(F2.reste(),F1),t2)
+        if(t1.degre == t2.degre):
+            return Union(F1.reste(),F2.reste(),union2Tid(t1,t2))
+                
+    else:
+        if(F1.estVide()):
+            return UnionFile(t.file(),F2)
+        if(F2.estVide()):
+            return UnionFile(t.file(),F1)
+        t1 = F1.minDegre()
+        t2 = F2.minDegre()
+        
+        if(t.degre<t1.degre and t.degre<t2.degre):
+            return ajoutMin(UnionFile(F1,F2),t)
+        if(t.degre==t1.degre and t.degre==t2.degre):
+            return ajoutMin(Union(F1.reste(),F2.reste(),union2Tid(t1,t2)),t)
+        if(t.degre == t1.degre and t.degre<t2.degre):
+            return Union(F1.reste(),F2,union2Tid(t,t1))
+        if(t.degre < t1.degre and t.degre==t2.degre):
+            return Union(F2.reste(),F1,union2Tid(t,t2))
+    
         
 l = list()
 a=cle.Cle("0x9c1f03a0d9cf510f2765bd0f226ff5dc")
@@ -114,39 +168,20 @@ c=cle.Cle("0x2e73d8ce4bd45923286e966bc8cf2d95")
 d=cle.Cle("0x767accd0c60c603f71a68be994019c7e")
 e=cle.Cle("0x34c63c08abab99722b945e57081288e7")
 f=cle.Cle("0x6d481adc2aeed025f0374a5982b5c23c")
-l.append(a)
-l.append(b)
-l.append(c)
-l.append(d)
-l.append(e)
-l.append(f)
-t1 = TournoisBino()
-t1.ConsIter(l[0:2])
-t2 = TournoisBino()
-t2.ConsIter(l[2:5])
-file = FileBinomial()
-file.AjoutTournois(t2)
-file.AjoutTournois(t1)
-
-A=cle.Cle("0xb26a633d855b8b0af4c692dda999ddcc")
-B=cle.Cle("0x31a872636ac53ed0dd00b3f27470bbb4")
-C=cle.Cle("0xd78cdbe82edab23a56f47f978fcbb8be")
-D=cle.Cle("0xe76f477e1431b3577ebf079fc7e4493e")
-E=cle.Cle("0x4c97cd1b0b09e387826e0d9d68d4071d")
-l = list()
-l.append(A)
-l.append(B)
-l.append(C)
-l.append(D)
-l.append(E)
-t1 = TournoisBino()
-t1.ConsIter(l[0:1])
-t2 = TournoisBino()
-t2.ConsIter(l[1:4])
-file2 = FileBinomial()
-file2.AjoutTournois(t2)
-file2.AjoutTournois(t1)
-file2.Union(file)
-
-for x in file2.tournois:
-    print(x.degre)
+t1 = TournoisBino(a)
+t2 = TournoisBino(b)
+t3 = TournoisBino(c)
+t4 = TournoisBino(d)
+t5 = TournoisBino(e)
+t6 = TournoisBino(f)
+l.extend([t1,t2,t3,t4,t5,t6])
+F = FileBinomial()
+F.ConsIter(l)
+print(F)
+for c in F.tournois:
+    print(c)
+    
+print(F.SuppMin())
+print(F)
+for c in F.tournois:
+    print(c)
